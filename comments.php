153698278a1302156257d886ec732e93ec74e395
Christine Will <ckwill90@gmail.com>
2023-04-08 12:34:51 -0400

    Removed unneeded script reference.

--- comments.php
+++ comments.php
@@ -2,9 +2,7 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-	
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="js/mrtasks.js"></script>
 	</head>
 	<body>
 	<h1>Customer Comments</h1>
